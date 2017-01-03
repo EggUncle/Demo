882a43cb67719e82b9b6fd7e363c559a3fa3e81b
EggUncle <cicadashadow@gmail.com>
2017-01-03 23:41:01 +0800

    利用反射和注解实现xutils的findviewbyid和setcontentview功能

---
+++ Demo/app/src/main/java/com/example/egguncle/demo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.egguncle.demo;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.example.egguncle.demo.myXutils.MyLayoutInject;
+import com.example.egguncle.demo.myXutils.MyUtils;
+import com.example.egguncle.demo.myXutils.MyViewInject;
+
+
+@MyLayoutInject(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+
+    @MyViewInject(R.id.btn)
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+   //     setContentView(R.layout.activity_main);
+
+        MyUtils.inject(this);
+       
+
+        btn.setBackgroundColor(Color.BLUE);
+
+    }
+}
