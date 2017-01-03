cd406e6b066b4d8cb7dac40ed5f0068bf8b5376b
EggUncle <cicadashadow@gmail.com>
2017-01-03 23:39:42 +0800

    利用反射和注解实现xutils的findviewbyid和setcontentview功能

---
+++ Demo/app/src/main/java/com/example/egguncle/demo/myXutils/MyUtils.java
@@ -0,0 +1,53 @@
+package com.example.egguncle.demo.myXutils;
+
+import android.content.Context;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Created by egguncle on 17-1-3.
+ */
+
+public class MyUtils {
+
+    public static void myViewInject(Context context) {
+        try {
+            Class<? extends Context> uiClass = context.getClass();
+
+            Field[] fields = uiClass.getDeclaredFields();
+            for (Field f : fields) {
+                MyViewInject annotation = f.getAnnotation(MyViewInject.class);
+
+                if (annotation != null) {
+                    int value = annotation.value();
+                    Method findViewById = uiClass.getMethod("findViewById", int.class);
+                    Object invoke = findViewById.invoke(context, value);
+
+                    //设置允许访问
+                    f.setAccessible(true);
+                    f.set(context, invoke);
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static void myLayoutInject(Context context) {
+        try {
+            Class<? extends Context> uiClass = context.getClass();
+
+            MyLayoutInject annotation = uiClass.getAnnotation(MyLayoutInject.class);
+            if (annotation != null) {
+                int value = annotation.value();
+                Method setContentView = uiClass.getMethod("setContentView", int.class);
+                setContentView.invoke(context,value);
+            }
+        } catch (Exception e) {
+
+        }
+
+
+    }
+}
